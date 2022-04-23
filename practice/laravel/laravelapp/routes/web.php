dd9680d31489b42fea253701106b36a08b960713
Ruben Rienstra <rotezdesign@gmail.com>
2022-04-23 15:33:00 +0200

    added routes and methods

--- practice/laravel/laravelapp/routes/web.php
+++ practice/laravel/laravelapp/routes/web.php
@@ -16,19 +16,27 @@ use App\Http\Controllers\PostsController;
 */
 
 // GET (for getting all blogs)
-Route::get('/blog', [PostsController::class, 'index']);
-Route::get('/article/{id?}', [PostsController::class, 'show']);
+Route::get('/blog', [PostsController::class, 'index'])->name('blog.index');
+// Make route parameter optional
+// Route::get('/blog/{id?}', [PostsController::class, 'show'])->whereNumber('id');
+// Route::get('/blog/{name}', [PostsController::class, 'show'])->where('name', '[A-Za-z]+');
+
+Route::get('/blog/{id}/{name}', [PostsController::class, 'show'])
+    ->whereNumber('id')
+    ->whereAlpha('name');
+
 
 // POST
-Route::get('/blog/create', [PostsController::class, 'create']);
-Route::post('/blog/', [PostsController::class, 'store']);
+Route::get('/blog/create', [PostsController::class, 'create'])->name('blog.create');
+Route::post('/blog/', [PostsController::class, 'store'])
+    ->name('blog.store');
 
 // PUT OR PATCH
-Route::get('/blog/edit/{id}', [PostsController::class, 'edit']);
-Route::patch('/blog/{id}', [PostsController::class, 'update']);
+Route::get('/blog/edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+Route::patch('/blog/{id}', [PostsController::class, 'update'])->name('blog.update');
 
 // DELETE
-Route::delete('/blog/{id}', [PostsController::class, 'destroy']);
+Route::delete('/blog/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
 
 // Route::resource('/blog', PostsController::class);
 
