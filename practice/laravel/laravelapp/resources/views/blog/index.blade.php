aab4ad7bf4c44708551417d2346c850ae5f79868
Ruben Rienstra <rotezdesign@gmail.com>
2022-04-23 11:28:48 +0200

    added controllers

---
+++ practice/laravel/laravelapp/resources/views/blog/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Index Method of Blgos
+</h1>
--- practice/laravel/laravelapp/routes/web.php
+++ practice/laravel/laravelapp/routes/web.php
@@ -1,17 +1,43 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    Methods
+
+    GET - Request a resource
+    POST - Create a new resource
+    PUT - Update a resource
+    PATCH - Modify a resource (only modifies values that have been changed)
+    DELETE  - Delete a resources
+    OPTIONS - Ask the server which verbs are allowed
 */
 
+// GET (for getting all blogs)
 Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/article/{id?}', [PostsController::class, 'show']);
+
+// POST
+Route::get('/blog/create', [PostsController::class, 'create']);
+Route::post('/blog/', [PostsController::class, 'store']);
+
+// PUT OR PATCH
+Route::get('/blog/edit/{id}', [PostsController::class, 'edit']);
+Route::patch('/blog/{id}', [PostsController::class, 'update']);
+
+// DELETE
+Route::delete('/blog/{id}', [PostsController::class, 'destroy']);
+
+// Route::resource('/blog', PostsController::class);
+
+// Route for invoke method
+Route::get('/', HomeController::class);
+
+// Multiple HTTP verbs
+// Route::match(['GET', 'POST'], '/blog', [PostsController::class, 'index']);
+// Route::any('/blog', [PostsController::class, 'index']);
+
+// Return view
+// Route::view('/blog', 'blog.index', ['name' => 'Ruben Rienstra']);
