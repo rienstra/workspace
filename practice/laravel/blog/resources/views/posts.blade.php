529ae1c7dc961b62987ad405385e4c3e4921a9b9
Ruben Rienstra <rotezdesign@gmail.com>
2022-04-23 16:42:23 +0200

    first blog commit

---
+++ practice/laravel/blog/resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus?
+        </p>
+    </article>
+    
+    <article>
+        <h1>My Second Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus?
+        </p>
+    </article>
+    
+    <article>
+        <h1>My Third Post</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam tempora, possimus at numquam similique facilis unde aliquid laborum quas quaerat et rem debitis provident nihil dolore aspernatur eum deserunt doloribus?
+        </p>
+    </article>
+</body>
