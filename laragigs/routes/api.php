a87ea570af53298dcf75f201cf17c4faa0b72858
Ruben Rienstra <rotezdesign@gmail.com>
2022-04-29 11:04:22 +0200

    laragis progress

--- laragigs/routes/api.php
+++ laragigs/routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// use /api/ in url
+Route::get('/posts', function () {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et minus, officiis non aperiam aut voluptatibus excepturi natus vero in accusamus, ex repudiandae esse magnam nesciunt id possimus, qui illo!'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et minus, officiis non aperiam aut voluptatibus excepturi natus vero in accusamus, ex repudiandae esse magnam nesciunt id possimus, qui illo!'
+            ]
+        ]
+    ]);
 });
+
+// Route::get('/hello', function () {
+//     return response('<h1>Hello World</h1>', 200)
+//         ->header('Content-Type', 'text/plain')
+//         ->header('foo', 'bar');
+// });
+
+// Route::get('/posts/{id}', function ($id) {
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function (Request $request) {
+//     return ($request->name . ' ' . $request->city);
+// });
